a917bf4632abd702cb70990250bbf34960f9c2cf
Mynagani Mahesh Kumar <147359586+Mahi209@users.noreply.github.com>
2025-07-13 16:37:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path={["/", "/login"]} element={<Login />} />
 
         <Route
           path="/dashboard"
