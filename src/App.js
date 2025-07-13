031cf4d0ce182bb7bad47f3125a42e4dfcb61762
Mynagani Mahesh Kumar <147359586+Mahi209@users.noreply.github.com>
2025-07-13 17:06:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/loginPage';
 import Dashboard from './components/Dashboard';
 import Employee from './components/Employee';
-import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/employee" element={<Employee />} />
         <Route path="/register" element={<Register />} />
         <Route path={["/", "/login"]} element={<Login />} />
 
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/employee"
-          element={
-            <ProtectedRoute>
-              <Employee />
-            </ProtectedRoute>
-          }
-        />
       </Routes>
     </div>
   );
